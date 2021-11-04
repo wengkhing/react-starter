33af0024e9860d94ce108d2a1e5bfc40d3ec2882
wengkhing <wengkhing@gmail.com>
2021-11-04 16:03:34 +0800

    setup github pipeline

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import { StaticRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { HelmetProvider } from 'react-helmet-async';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import store from './store/store';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(
+    <Provider store={store}>
+      <HelmetProvider>
+        <StaticRouter>
+          <App />
+        </StaticRouter>
+      </HelmetProvider>
+    </Provider>
+  );
+  const linkElement = screen.getByText(/save to reload/i);
   expect(linkElement).toBeInTheDocument();
 });
