37f26688e9997801c10ecd492e23c585d2dd619d
wengkhing <wengkhing@gmail.com>
2021-11-11 22:29:31 +0800

    use app env instead of node env

--- config/app.config.js
+++ config/app.config.js
@@ -6,14 +6,14 @@ const region = 'ap-southeast-1';
 // Do not change the below
 
 const bucketName = `${appName}-assets`;
-const environment = process.env.NODE_ENV === 'production' ? 'prod' : 'dev';
+const environment = process.env.APP_ENV === 'production' ? 'prod' : 'dev';
 const deployedDomain = `https://${bucketName}-${environment}.s3.${region}.amazonaws.com`;
 const staticDomain =
   process.env.IS_OFFLINE === 'true'
     ? 'http://localhost:8080'
     : deployedDomain;
 
-console.log('NODE_ENV:', process.env.NODE_ENV);
+console.log('APP_ENV:', process.env.APP_ENV);
 console.log('bucketName:', bucketName);
 console.log('environment:', environment);
 console.log('staticDomain:', staticDomain);
--- script/deploy.sh
+++ script/deploy.sh
@@ -5,14 +5,14 @@ DEPLOY_ENV=${1:-dev}
 case $DEPLOY_ENV in
   prod)
     export NODE_ENV=production
+    export APP_ENV=production
     ;;
   *)
     export NODE_ENV=development
+    export APP_ENV=development
     ;;
 esac
 
 yarn build
-echo "NODE_ENV: $NODE_ENV"
 yarn upload $DEPLOY_ENV &&
-echo "NODE_ENV: $NODE_ENV"
 npx sls deploy --stage $DEPLOY_ENV
